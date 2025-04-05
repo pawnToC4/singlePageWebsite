04cb1a22c9e993e48cdb459107c1ae8a57bdd79c
pawnToC4 <lionelpena91@gmail.com>
2025-04-05 03:46:47 -0400

    My version of the project
    Code I wrote from the ground up based on Coursera sample webpage image.

--- script.js
+++ script.js
@@ -1,27 +1,30 @@
-function addRecommendation() {
-  // Get the message of the new recommendation
-  let recommendation = document.getElementById("new_recommendation");
-  // If the user has left a recommendation, display a pop-up
-  if (recommendation.value != null && recommendation.value.trim() != "") {
-    console.log("New recommendation added");
-    //Call showPopup here
-    showPopup(true);
-    // Create a new 'recommendation' element and set it's value to the user's message
-    var element = document.createElement("div");
-    element.setAttribute("class","recommendation");
-    element.innerHTML = "\<span\>&#8220;\</span\>" + recommendation.value + "\<span\>&#8221;\</span\>";
-    // Add this element to the end of the list of recommendations
-    document.getElementById("all_recommendations").appendChild(element); 
-    
-    // Reset the value of the textarea
-    recommendation.value = "";
-  }
-}
+const postQuote = ()=>{
+    if(confirm("Would like to submit quote?")){
+        //Get user input    
+        const quoteElement = document.getElementById("recommendation_quote");
+        const quote = quoteElement.value;
+        const userNameElement = document.getElementById("userName");
+        let userName = userNameElement.value.trim();
+        if(userName !== ""){
+            userName = " \u2015 " + userName;
+            userNameElement.value = "";
+        }
 
-function showPopup(bool) {
-  if (bool) {
-    document.getElementById('popup').style.visibility = 'visible'
-  } else {
-    document.getElementById('popup').style.visibility = 'hidden'
-  }
-}
+        //Create and prepare HTML containers for user input
+        const recommendations = document.getElementById("recommendation_container");    
+        const recommendation_item = document.createElement("div");
+        recommendations.appendChild(recommendation_item);
+        recommendation_item.setAttribute("class","recommendation_item");
+
+        //Create and prepare HTML elements to hold user input
+        const post_quote = document.createElement("p");
+        const quote_node = document.createTextNode("\u201C"+ quote +"\u201D" + userName);
+        recommendation_item.appendChild(post_quote);
+        post_quote.appendChild(quote_node);
+
+        //Upon completion, clear user input from Input field
+        quoteElement.value = "";
+    }
+    //Return false to prevent page refresh
+    return false;
+};
